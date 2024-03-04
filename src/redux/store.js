539d7128a358d2173d4164c8c7ed054eb180d0a7
Mykola <53128883+Nikolaus-B@users.noreply.github.com>
2024-03-04 21:42:52 +0200

    create redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userReducer } from './user/userSlice';
+import { productsReducer } from './products/productsSlice';
+import { exercisesReducer } from './exercises/exercisesSlice';
+import { diaryReducer } from './diary/diarySlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    products: productsReducer,
+    exercises: exercisesReducer,
+    diary: diaryReducer,
   },
 });
