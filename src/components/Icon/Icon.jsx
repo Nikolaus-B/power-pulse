9f9ab9750aa4fac0ffba3f06df4446730ced4ec3
Mykola <53128883+Nikolaus-B@users.noreply.github.com>
2024-03-05 22:13:40 +0200

    add icon

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -2,10 +2,10 @@ import sprite from '../../img/pulse-icons.svg';
 
 import React from 'react';
 
-export const Icon = ({ width, height, iconId }) => {
+export const Icon = ({ width, height, iconid }) => {
   return (
     <svg width={width} height={height}>
-      <use href={`${sprite}#${iconId}`}></use>
+      <use href={`${sprite}#${iconid}`}></use>
     </svg>
   );
 };
