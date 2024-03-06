293db99e99138faa7acc88e24052403e89841016
okaisym <okaisim@gmail.com>
2024-03-06 23:01:05 +0200

    modal added

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -62,6 +62,26 @@ padding: 5px;
     font-size: 1.8rem;
 `;
 
+export const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    border: '1px solid',
+    borderRadius: '12px',
+    borderColor: 'rgba(239, 237, 232, 0.2)',
+    backgroundColor: 'rgba(16, 16, 15, 1)',
+    padding: 0,
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.707)',
+    zIndex: 1001,
+  },
+};
+
 // export const LogoContainer = styled.div`
 //   display: flex;
 //   align-items: center;
