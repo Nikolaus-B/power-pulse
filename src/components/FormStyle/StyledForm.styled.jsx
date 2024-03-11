60b0b978e74265254be58f8159579f75f1c5e158
SergeyReznickyy <131711981+SergeyReznickyy@users.noreply.github.com>
2024-03-11 22:24:04 +0200

    Дщдав бібліотеку волідації

--- src/components/FormStyle/StyledForm.styled.jsx
+++ src/components/FormStyle/StyledForm.styled.jsx
@@ -174,17 +174,16 @@ export const IconsIn = styled.svg`
   height: 20px;
   position: absolute;
   left: 320px;
-  bottom: 205px;
+  top: 225px;
 
   @media screen and (min-width: 768px) {
     position: absolute;
     left: 362px;
-    bottom: 254px;
+    top: 266px;
   }
 
   @media screen and (min-width: 1440px) {
     position: absolute;
     left: 426px;
-    bottom: 243px;
   }
 `;
