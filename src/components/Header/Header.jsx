49582fe15cfdc1d3a33a3f44c61701a2f04f731a
Mykola <53128883+Nikolaus-B@users.noreply.github.com>
2024-03-11 19:58:04 +0200

    Revert "Merge pull request #23 from Nikolaus-B/header"
    This reverts commit f116f3fb58126fe48fd6d0a78eb0fab98f189046, reversing
    changes made to 5513d8c35ea1d0ae935baa0af6fc160dae620161.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,21 @@
-import {
-  HeaderContainer,
-  NavBtn,
-  UserContainer,
-  LogoLink,
-  HeaderWrapper,
-} from './Header.styled';
-import { Icon } from 'components/Icon/Icon';
-import { UserMenu } from 'components/UserMenu/UserMenu';
-import { MobileMenu } from 'components/MobileMenu/MobileMenu';
-import { useMediaQuery } from 'react-responsive';
+import { NavLink } from 'react-router-dom';
 
 export const Header = () => {
-  const isMobile = useMediaQuery({ maxWidth: 767 });
-  const isTablet = useMediaQuery({ maxWidth: 1439 });
-
   return (
-    <HeaderWrapper>
-      <HeaderContainer>
-        <LogoLink to="/">
-          <Icon iconid={'icon-Logo'} width={126} height={13} />
-        </LogoLink>
-
-        <UserContainer>
-          <UserMenu />
-
-          {(isMobile || isTablet) && (
-            <NavBtn>
-              <Icon iconid={'icon-Icon'} width={24} height={16} />
-            </NavBtn>
-          )}
-
-          <MobileMenu />
-        </UserContainer>
-      </HeaderContainer>
-    </HeaderWrapper>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/diary">Diary</NavLink>
+          </li>
+          <li>
+            <NavLink to="/product">Product</NavLink>
+          </li>
+          <li>
+            <NavLink to="/exercises">Exercises</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
