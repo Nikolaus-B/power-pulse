d62806bae763f5bff142aacf646b0ade9070f69e
SvitLana1990 <svitlana112@outlook.es>
2024-03-13 18:09:31 +0100

    add cursor:pointer

--- src/components/Exercises/ExercisesCatigories/ExercisesCategories.styled.js
+++ src/components/Exercises/ExercisesCatigories/ExercisesCategories.styled.js
@@ -1,37 +1,40 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Ul = styled.ul`
-    display: flex;
-    gap: 28px;
-    margin-bottom: 40px;
-    margin-top: 20px;
+  display: flex;
+  gap: 28px;
+  margin-bottom: 40px;
+  margin-top: 20px;
 
-    @media screen and (min-width: 768px) {
-        margin: 0;
-        gap: 32px;
-        height: 32px;
-    }
+  @media screen and (min-width: 768px) {
+    margin: 0;
+    gap: 32px;
+    height: 32px;
+  }
 `;
 
 export const Category = styled.p`
-    position: relative;
-    color: ${props => props.selected ? 'var(--white-color)' : 'var(--text-categories-color)'};
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 1.29;
-    &::after {
+  position: relative;
+  color: ${props =>
+    props.selected ? 'var(--white-color)' : 'var(--text-categories-color)'};
+  font-weight: 400;
+  font-size: 14px;
+  cursor: pointer;
+  line-height: 1.29;
+  &::after {
     content: '';
     position: absolute;
     left: 0;
     bottom: -8px;
     width: 100%;
-    height: ${(props) => (props.selected ? '4px' : '0')};
-    background-color: ${(props) => (props.selected ? 'var(--orange_1-color)' : 'transparent')};
+    height: ${props => (props.selected ? '4px' : '0')};
+    background-color: ${props =>
+      props.selected ? 'var(--orange_1-color)' : 'transparent'};
     border-radius: 2px;
-    }
+  }
 
-    @media screen and (min-width: 768px) and (max-width: 1439px) {
-        font-size: 16px;
-        line-height: 1.5;
-    }
+  @media screen and (min-width: 768px) and (max-width: 1439px) {
+    font-size: 16px;
+    line-height: 1.5;
+  }
 `;
